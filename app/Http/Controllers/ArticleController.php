02da28e7ad3a132162b3901d90480c95d63e920f
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-23 21:58:49 +0200

    creating articles in the controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,5 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller {
+
+    public function create(Request $request, News $news) {
+
+        $article = Article::create([
+            'content' => $request->content,
+            'news_id' => $news->id,
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json($article);
+    }
+
+    public function listArticles(News $news) {
+        $articles = $news->articles;
+        return response()->json($articles);
+    }
     
 }
