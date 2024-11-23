29b5bebbe42fd3e9f6e7b31a0d1a8c567d29115f
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-23 18:08:46 +0200

    creating news

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 use App\Models\News;
 
 class NewsController extends Controller {
-    
+
+    public function create(Request $request) {
+        $news = News::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json($news);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\testController;
+use App\Http\Controllers\Admin\NewsController;
 
 Route::get("/test", [testController::class, "test_api"]);
+Route::post('news', [NewsController::class, 'create']);
 
 ?>
