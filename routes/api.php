a1015184e6a463843a2e9e309c0caa2c9be6a840
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-25 21:59:52 +0200

    implementing routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\testController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('user', [AuthController::class, 'user']);
+Route::middleware('auth:api')->post('logout', [AuthController::class, 'logout']);
 
 Route::get("/test", [testController::class, "test_api"]);
 Route::middleware('auth')->group(function () {
