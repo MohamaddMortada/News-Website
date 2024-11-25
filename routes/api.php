bcbce565b81657062f1708b3d92008293cdaf3a0
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-25 21:15:53 +0200

    adding middleware for the routes

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\ArticleController;
 
 
 Route::get("/test", [testController::class, "test_api"]);
-Route::prefix("/news")->group(function() {
-    Route::post('/', [NewsController::class, 'create']);
-    Route::put('/{id}', [NewsController::class, 'edit']);
-    Route::delete('/{id}', [NewsController::class, 'remove']);
-    Route::post('/{id}/restrict', [NewsController::class, 'restrict']);
-    Route::post('{id}/articles', [ArticleController::class, 'create']);
-    Route::get('{id}/articles', [ArticleController::class, 'listArticles']);
+Route::middleware('auth')->group(function () {
+    Route::post('/news', [NewsController::class, 'create']);
+    Route::put('/news/{id}', [NewsController::class, 'edit']);
+    Route::delete('/news/{id}', [NewsController::class, 'remove']);
+    Route::post('/restrict', [NewsController::class, 'restrict']);
+    Route::post('news/{id}/articles', [ArticleController::class, 'create']);
+    Route::get('news/{id}/articles', [ArticleController::class, 'listArticles']);
 });
 ?>
